ef69844cfdf0963177af256f3be9ea108c5dffe6
josephnewell90 <josephnewell90@gmail.com>
2015-09-24 09:47:03 -0600

    Put the longestString function in the right place It was in the arraySum file......

--- problems/longest-string.js
+++ problems/longest-string.js
@@ -1,7 +1,18 @@
 'use strict';
 
 function longestString(arr) {
-
+  var longestString = '';
+  var currentString = '';
+  for (var i = 0; i < arr.length; i++)
+  {
+    currentString = arr[i];
+    console.log(currentString);
+    if (typeof currentString === 'string')
+    {
+      longestString += arr[i];
+    }
+    console.log(longestString);
+  }
 }
 
 module.exports = longestString;
