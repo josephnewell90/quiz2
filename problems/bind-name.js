78775db2265dda7c8b87c6f9ffce354cfc050fd8
josephnewell90 <josephnewell90@gmail.com>
2015-09-25 14:11:47 -0600

    bind-name passing test
    Honestly, I really need to learn a ton more about .bind. I managed to
    fiddle around and get it to work, but I don't completely understand why it
    needs to be declared as a variable, why I should use it instead of just
    calling the function itself (especially since we aren't explicitly
    defining this) and how it is different from just declaring a new function
    by itself other than being more terse.

--- problems/bind-name.js
+++ problems/bind-name.js
@@ -1,7 +1,9 @@
 'use strict';
 
 function bindName(firstName, lastName, func) {
-
+  var fullName = firstName + " " + lastName;
+  var greetingBind = func.bind(null, fullName);
+  return greetingBind;
 }
 
 module.exports = bindName;
