defdc7648c4de18e2c67b5abb0745f8b983a6a41
josephnewell90 <josephnewell90@gmail.com>
2015-09-25 13:09:04 -0600

    arraySum finished
    Passes all tests. Had to use parseInt because it would randomly treat the
    numbers as strings resulting in sums such as "253045"

--- problems/array-sum.js
+++ problems/array-sum.js
@@ -1,7 +1,29 @@
 'use strict';
 
 function arraySum(arr) {
+  var sum = 0;
+  var subSum = 0;
+  var subArray = null;
+  var arrayChecker = [];
 
+  for (var i = 0; i < arr.length; i++)
+  {
+    subSum = 0;   //Reset the subSum
+    arrayChecker = arr[i];
+    if (Array.isArray(arrayChecker))
+    {
+      subArray = arr[i];
+      for (var count = 0; count < subArray.length; count++)
+      {
+        subSum += parseInt(subArray[count], 10);
+      }
+      sum += parseInt(subSum, 10);
+    }
+    else {
+      sum += arr[i];
+    }
+  }
+  return sum;
 }
 
 module.exports = arraySum;
